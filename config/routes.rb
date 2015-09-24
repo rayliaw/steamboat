3cf69f53a17e02ffa930d89447429d62645c8f87
rayliaw <ray.liaw@gmail.com>
2015-09-23 20:22:59 -0700

    home and test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/test'
+  get 'test', to: 'static_pages#test', as: :test
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
